2e292784c41ccff75d1072deb941da3841e7ec43
Nicholas Triano <nicholastriano1999@gmail.com>
2023-04-14 12:44:39 -0400

    fixed error message position

--- components/AlertMessage.jsx
+++ components/AlertMessage.jsx
@@ -6,16 +6,16 @@ import { motion } from "framer-motion";
 const AlertMessage = ({ success, message }) => {
   return !success ? (
     <motion.div
-      initial={{ y: -25, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      className="fixed left-[50%] top-5 flex translate-x-[-50%] items-center justify-center gap-6 rounded-lg bg-red-600 px-6 py-2 text-white"
+      initial={{ y: -25, x: "-50%", opacity: 0 }}
+      animate={{ y: 0, x: "-50%", opacity: 1 }}
+      className="fixed left-[50%] top-5 flex items-center justify-center gap-4 rounded-lg bg-red-600 px-6 py-2 text-white"
     >
       <FiAlertCircle />
       <h1>{message}</h1>
     </motion.div>
   ) : (
     <motion.div
-      className="fixed left-[50%] top-5 flex translate-x-[-50%] items-center justify-center gap-4 rounded-lg bg-green-600 px-6 py-2 text-white"
+      className="fixed left-[50%] top-5 flex items-center justify-center gap-4 rounded-lg bg-green-600 px-6 py-2 text-white"
       initial={{ y: -25, x: "-50%", opacity: 0 }}
       animate={{ y: 0, x: "-50%", opacity: 1 }}
       transition={{ duration: 0.5 }}
