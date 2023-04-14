3a7b198883e198b565ebd3b1184253ee37b93996
Nicholas Triano <nicholastriano1999@gmail.com>
2023-04-14 15:37:27 -0400

    social icons in nav

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import NavItem from "./NavItem";
 import HomeLogo from "./HomeLogo";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 const NavBar = () => {
   const navItems = [
@@ -11,8 +13,52 @@ const NavBar = () => {
 
   return (
     <nav className="flex w-full items-center justify-center px-12 pt-16">
-      <ul className="flex w-full max-w-[1200px] items-center justify-between">
+      <ul className="flex w-full max-w-[1200px] flex-col items-center justify-between gap-4 md:flex-row">
         <HomeLogo />
+        <div className="flex items-center justify-center gap-4">
+          <motion.a
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ type: "spring", duration: 0.5, delay: 1.25 }}
+            href="https://github.com/RealNickTriano"
+            target="_blank"
+          >
+            <Image
+              src="/icons8-github.svg"
+              alt="github_icon"
+              width={48}
+              height={48}
+            />
+          </motion.a>
+          <motion.a
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ type: "spring", duration: 0.5, delay: 1.5 }}
+            href="https://www.linkedin.com/in/nicholastriano/"
+            target="_blank"
+          >
+            <Image
+              src="/icons8-linkedin.svg"
+              alt="linkedin_icon"
+              width={48}
+              height={48}
+            />
+          </motion.a>
+          <motion.a
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ type: "spring", duration: 0.5, delay: 1.75 }}
+            href="https://twitter.com/nKoalaBear"
+            target="_blank"
+          >
+            <Image
+              src="/icons8-twitter.svg"
+              alt="twitter_icon"
+              width={48}
+              height={48}
+            />
+          </motion.a>
+        </div>
         <div className="hidden items-center justify-center gap-12 lg:flex">
           {navItems.map((item, idx) => {
             return (
